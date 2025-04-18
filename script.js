fd7e5d21fffc90877b9e110d1fc82e39897ebc08
FarahAbughali <farahabughali129@gmail.com>
2025-04-18 22:33:25 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    Scrollbar.init(document.querySelector('body'), {
-        damping: 0.1,
-        renderByPixels: true,
-        continuousScrolling: true
-    });
-});
